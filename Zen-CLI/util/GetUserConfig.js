082acf11f0514bab3aa0d96a9cea7b74b1eb008d
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2018-10-19 10:06:11 +0800

    完善注释

--- Zen-CLI/util/GetUserConfig.js
+++ Zen-CLI/util/GetUserConfig.js
@@ -10,6 +10,8 @@ module.exports = function( userDir ){
     config = {};
   }
 
+  // 如果有 pipe 配置, 则将 config 配置视为当前项目的默认配置使用
+  // 否则将 config 当做唯一配置使用
   if( config.pipe && config.pipe.length ){
     config = config.pipe.map( cfg => {
       return Object.$assign( {}, defaultConfig.config, config.config, cfg );
