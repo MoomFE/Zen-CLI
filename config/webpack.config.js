bc773a9a30f8f236b35d0077bfed724da054c4f1
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2018-10-20 19:10:48 +0800

    新增 @babel/polyfill, 根据使用的代码自动添加 polyfill

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -14,7 +14,9 @@ module.exports = {
         use:{
           loader: 'babel-loader',
           options: {
-            presets: [ '@babel/preset-env' ],
+            presets: [
+              [ '@babel/preset-env', { useBuiltIns: 'usage' } ]
+            ],
             plugins: [ '@babel/plugin-transform-runtime' ]
           }
         }
