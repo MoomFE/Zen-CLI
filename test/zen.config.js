e9e2a3c7a701f7b21abda90600671fad912fb2d8
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2018-10-22 14:40:42 +0800

    修复因缺少 @babel/runtime 而打包错误的问题

--- test/zen.config.js
+++ test/zen.config.js
@@ -1,18 +1,5 @@
 
 
 module.exports = {
-  mode: false,
-  banner:
-`/*!
- * Zen.js
- * (c) 2003-2018 123
- * Released under the Apache License.
- * 
- * Author: Zhang_Wei
- * Updata: YYYY-MM-DD
- * 
- * Path: $path
- * Warn: 本代码使用前端工具构建, 请勿修改.
- */
- `
+  mode: true
 }
