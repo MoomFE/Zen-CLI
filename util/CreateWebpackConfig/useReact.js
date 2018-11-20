a116f84be0c62cb90740bb7729e87d078732c784
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2018-11-20 19:42:14 +0800

    优化创建 jsx rule 的逻辑

--- util/CreateWebpackConfig/useReact.js
+++ util/CreateWebpackConfig/useReact.js
@@ -1,14 +1,26 @@
+const DefaultWebpackConfig = require('../../config/webpack.config');
 
 
 module.exports = function( NewWebpackConfig, config ){
   if( config.useReact ){
 
-    const JS = NewWebpackConfig.module.rules[0];
+    // 继承至解析 js 的规则
+    const JS = DefaultWebpackConfig.module.rules[0];
+
+    // 新建一条专门解析 jsx 的规则
     const JSX = Object.$assign( {}, JS, {
       test: /\.jsx$/
     });
 
-    JSX.use.options.presets.push([ '@babel/preset-react' ]);
+    // 解析 jsx
+    JSX.use.options.presets.push([
+      '@babel/preset-react'
+    ]);
+
+    // 如果需要自动添加 polyfill
+    if( config.autoPolyfill ){
+      JSX.use.options.presets[0].push({ useBuiltIns: 'usage' });
+    }
 
     NewWebpackConfig.module.rules.push( JSX );
 
