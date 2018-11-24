330515656ed844a5091761604c78dd5841e34812
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2018-11-24 16:56:20 +0800

    优化 css 处理

--- util/CreateWebpackConfig/util/GetProcessing.js
+++ util/CreateWebpackConfig/util/GetProcessing.js
@@ -2,25 +2,25 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const GetPostcssLoader = require('./GetPostcssLoader');
 
 
-module.exports = function( name, test ){
+module.exports = function( name, test, isCss ){
 
   const LowerName = name.toLowerCase();
 
   return ( NewWebpackConfig, config ) => {
 
-    // 没有使用 less / sass
-    if( !config[ 'use' + name ] ) return;
+    if( !isCss ){
+      // 没有使用 less / sass
+      if( !config[ 'use' + name ] ) return;
+    }
 
     // 将 css 内置在 js 中
     if( config.builtInCss ){
       const use = [
-        'style-loader', 'css-loader', `${ LowerName }-loader`,
-        GetPostcssLoader()
-      ];
-
-      if( config.useVue ){
-        use.unshift('vue-style-loader');
-      }
+        'style-loader', 'css-loader',
+        isCss ? '' : `${ LowerName }-loader`,
+        GetPostcssLoader(),
+        config.useVue ? 'vue-style-loader' : ''
+      ].$deleteValue('');;
 
       return NewWebpackConfig.module.rules.push({ test, use });
     }
@@ -28,18 +28,22 @@ module.exports = function( name, test ){
     const options = {
       fallback: 'style-loader',
       use: [
-        'css-loader', `${ LowerName }-loader`,
-        GetPostcssLoader()
-      ]
+        'css-loader',
+        isCss ? '' : `${ LowerName }-loader`,
+        GetPostcssLoader(),
+        config.useVue ? 'vue-style-loader' : ''
+      ].$deleteValue('')
     };
-
-    if( config.useVue ){
-      options.use.unshift('vue-style-loader');
-    }
 
     NewWebpackConfig.module.rules.push({
       test,
       use: ExtractTextPlugin.extract( options )
     });
+
+    if( isCss ){
+      NewWebpackConfig.plugins.push(
+        new ExtractTextPlugin( config.Plugin_ExtractTextPluginOptions )
+      );
+    }
   };
 }
