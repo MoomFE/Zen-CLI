871c4c612e43dba281ecf8f056dfb93db6fe15bb
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2018-11-24 16:02:10 +0800

    优化 css 处理

--- util/CreateWebpackConfig/builtInCss.js
+++ util/CreateWebpackConfig/builtInCss.js
@@ -1,47 +1,29 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const GetPostcssLoader = require('./util/GetPostcssLoader');
 
+
 module.exports = function( NewWebpackConfig, config ){
+  const test = /\.css$/;
 
   // 将 css 内置在 js 中
   if( config.builtInCss ){
-    return NewWebpackConfig.module.rules.push(
-      GetRule( config )
-    );
+    const use = [
+      'style-loader', 'css-loader',
+      GetPostcssLoader()
+    ];
+
+    if( config.useVue ){
+      use.unshift('vue-style-loader');
+    }
+
+    return NewWebpackConfig.module.rules.push({ test, use });
   }
 
-  NewWebpackConfig.plugins.push(
-    new ExtractTextPlugin( config.Plugin_ExtractTextPluginOptions )
-  );
-
-  NewWebpackConfig.module.rules.push(
-    GetRule2( config )
-  );
-}
-
-
-
-
-function GetRule( config ){
-  const rule = {
-    test: /\.css$/,
-    use: [
-      'style-loader', 'css-loader', GetPostcssLoader()
-    ]
-  };
-
-  if( config.useVue ){
-    rule.use.unshift('vue-style-loader');
-  }
-
-  return rule;
-}
-
-function GetRule2( config ){
   const options = {
     fallback: 'style-loader',
     use: [
-      'css-loader', GetPostcssLoader()
+      'css-loader',
+      GetPostcssLoader()
     ]
   };
 
@@ -49,8 +31,12 @@ function GetRule2( config ){
     options.use.unshift('vue-style-loader');
   }
 
-  return {
-    test: /\.css$/,
+  NewWebpackConfig.module.rules.push({
+    test,
     use: ExtractTextPlugin.extract( options )
-  };
+  });
+
+  NewWebpackConfig.plugins.push(
+    new ExtractTextPlugin( config.Plugin_ExtractTextPluginOptions )
+  );
 }
