9ff9bbab8e4d61d0e1caee016ed8228219bbe71d
Zhang-Wei-666 <994039348@qq.com>
2018-12-17 23:44:07 +0800

    重置 outputHtmlOptions 配置项 filename 文件路径

--- util/CreateWebpackConfig/14. OutputHtml.js
+++ util/CreateWebpackConfig/14. OutputHtml.js
@@ -1,10 +1,19 @@
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 
 module.exports = function( webpack, config ){
   if( config.outputHtml ){
+
+    const { outputHtmlOptions } = config;
+
+    if( outputHtmlOptions && outputHtmlOptions.filename ){
+      path.resolve( global.userDir, outputHtmlOptions.filename );
+    }
+
     webpack.plugins.push(
-      new htmlWebpackPlugin( config.outputHtmlOptions )
+      new htmlWebpackPlugin( outputHtmlOptions )
     );
+
   }
 }
